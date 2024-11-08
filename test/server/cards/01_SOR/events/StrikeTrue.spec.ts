8fe97c6f13fb85911d09733d3d719b89d24de5de
Angel de Jesús Ruiz Cerrillo <ing.angelruizcerrillo@gmail.com>
2024-11-07 22:19:34 -0600

    Strike true tests adjustments

--- test/server/cards/01_SOR/events/StrikeTrue.spec.ts
+++ test/server/cards/01_SOR/events/StrikeTrue.spec.ts
@@ -18,18 +18,6 @@ describe('Strike True', function() {
             it('should deal damage to an enemy unit equals to a friendly unit power', function () {
                 const { context } = contextRef;
 
-                // Enemy unit is defeated by the ability
-                context.player1.clickCard(context.strikeTrue);
-                expect(context.player1).toBeAbleToSelectExactly([context.wampa, context.battlefieldMarine]);
-                context.player1.clickCard(context.wampa);
-                expect(context.player1).toBeAbleToSelectExactly([context.tielnFighter, context.specforceSoldier, context.atst]);
-                context.player1.clickCard(context.tielnFighter);
-                expect(context.tielnFighter).toBeInLocation('discard');
-            });
-
-            it('should deal damage to an enemy unit equals to a friendly unit power', function () {
-                const { context } = contextRef;
-
                 // Enemy unit is not defeated by the ability
                 context.player1.clickCard(context.strikeTrue);
                 expect(context.player1).toBeAbleToSelectExactly([context.wampa, context.battlefieldMarine]);
@@ -48,8 +36,7 @@ describe('Strike True', function() {
                         hand: ['strike-true']
                     },
                     player2: {
-                        groundArena: ['specforce-soldier', 'atst'],
-                        spaceArena: ['tieln-fighter']
+                        hand: ['wampa', 'battlefield-marine']
                     }
                 });
             });
@@ -58,6 +45,29 @@ describe('Strike True', function() {
                 const { context } = contextRef;
 
                 // Play the event if there is no friendly units
+                context.player1.clickCard(context.strikeTrue);
+                expect(context.player2).toBeActivePlayer();
+            });
+        });
+
+        describe('Strike True\'s ability', function() {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['strike-true'],
+                        groundArena: ['specforce-soldier', 'atst']
+                    },
+                    player2: {
+                        hand: ['wampa']
+                    }
+                });
+            });
+
+            it('should be playable even if there is no enemy units ', function () {
+                const { context } = contextRef;
+
+                // Play the event if there is no enemy units
                 context.player1.clickCard(context.strikeTrue);
                 expect(context.player2).toBeActivePlayer();
             });

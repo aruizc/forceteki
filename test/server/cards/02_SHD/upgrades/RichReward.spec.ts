eb387c87aec49eac85716dad4f186b7efc9728d6
aruizc <ing.angelruizcerrillo@gmail.com>
2024-11-12 13:30:24 -0600

    Rich Reward unit testing refactoring

--- test/server/cards/02_SHD/upgrades/RichReward.spec.ts
+++ test/server/cards/02_SHD/upgrades/RichReward.spec.ts
@@ -6,10 +6,7 @@ describe('Rich Reward', function() {
                     phase: 'action',
                     player1: {
                         groundArena: [
-                            { card: 'battlefield-marine', upgrades: ['rich-reward'] },
-                            { card: 'atst' },
-                            { card: 'yoda#old-master' }
-                        ]
+                            { card: 'battlefield-marine', upgrades: ['rich-reward'] }, 'atst', 'yoda#old-master']
                     },
                     player2: {
                         groundArena: ['wampa'],
@@ -25,11 +22,46 @@ describe('Rich Reward', function() {
                 expect(context.player2).toBeAbleToSelectExactly([context.wampa, context.atst, context.yoda, context.concordDawnInterceptors]);
                 expect(context.player2).toHavePassAbilityButton();
                 expect(context.player2).toHavePrompt('Select 2 cards');
+
                 context.player2.clickCard(context.wampa);
                 context.player2.clickCard(context.concordDawnInterceptors);
                 context.player2.clickPrompt('Done');
+
                 expect(context.wampa).toHaveExactUpgradeNames(['experience']);
                 expect(context.concordDawnInterceptors).toHaveExactUpgradeNames(['experience']);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should give Experience token only to two units even if more units are selected', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: [
+                            { card: 'battlefield-marine', upgrades: ['rich-reward'] }, 'atst', 'yoda#old-master']
+                    },
+                    player2: {
+                        groundArena: ['wampa'],
+                        spaceArena: ['concord-dawn-interceptors']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.battlefieldMarine);
+                context.player1.clickCard(context.wampa);
+
+                expect(context.player2).toBeAbleToSelectExactly([context.wampa, context.atst, context.yoda, context.concordDawnInterceptors]);
+                expect(context.player2).toHavePassAbilityButton();
+                expect(context.player2).toHavePrompt('Select 2 cards');
+
+                context.player2.clickCard(context.wampa);
+                context.player2.clickCard(context.concordDawnInterceptors);
+                context.player2.clickCardNonChecking(context.atst);
+                context.player2.clickPrompt('Done');
+
+                expect(context.wampa).toHaveExactUpgradeNames(['experience']);
+                expect(context.concordDawnInterceptors).toHaveExactUpgradeNames(['experience']);
+                expect(context.atst.isUpgraded()).toBeFalse();
                 expect(context.player2).toBeActivePlayer();
             });
 
@@ -38,10 +70,7 @@ describe('Rich Reward', function() {
                     phase: 'action',
                     player1: {
                         groundArena: [
-                            { card: 'battlefield-marine', upgrades: ['rich-reward'] },
-                            { card: 'atst' },
-                            { card: 'yoda#old-master' }
-                        ]
+                            { card: 'battlefield-marine', upgrades: ['rich-reward'] }, 'atst', 'yoda#old-master']
                     },
                     player2: {
                         groundArena: ['wampa'],
@@ -57,37 +86,11 @@ describe('Rich Reward', function() {
                 expect(context.player2).toBeAbleToSelectExactly([context.wampa, context.atst, context.yoda, context.concordDawnInterceptors]);
                 expect(context.player2).toHavePassAbilityButton();
                 expect(context.player2).toHavePrompt('Select 2 cards');
+
                 context.player2.clickCard(context.yoda);
                 context.player2.clickPrompt('Done');
+
                 expect(context.yoda).toHaveExactUpgradeNames(['experience']);
-                expect(context.player2).toBeActivePlayer();
-            });
-
-            it('should be able to not collect the bounty', function () {
-                contextRef.setupTest({
-                    phase: 'action',
-                    player1: {
-                        groundArena: [
-                            { card: 'battlefield-marine', upgrades: ['rich-reward'] },
-                            { card: 'atst' },
-                            { card: 'yoda#old-master' }
-                        ]
-                    },
-                    player2: {
-                        groundArena: ['wampa'],
-                        spaceArena: ['concord-dawn-interceptors']
-                    }
-                });
-
-                const { context } = contextRef;
-
-                context.player1.clickCard(context.battlefieldMarine);
-                context.player1.clickCard(context.wampa);
-
-                expect(context.player2).toBeAbleToSelectExactly([context.wampa, context.atst, context.yoda, context.concordDawnInterceptors]);
-                expect(context.player2).toHavePassAbilityButton();
-                expect(context.player2).toHavePrompt('Select 2 cards');
-                context.player2.clickPrompt('Pass ability');
                 expect(context.player2).toBeActivePlayer();
             });
         });
